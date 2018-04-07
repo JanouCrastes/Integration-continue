fd9e7175fe1be233cfd2147f676b4e95a19750de
victorien <victorien.baty@epsi.fr>
2018-04-07 11:30:06 +0200

    implement Product.java

--- src/main/java/src/Product.java
+++ src/main/java/src/Product.java
@@ -1,6 +1,30 @@
 package src;
 
 public class Product {
+    public double getPrix() {
+        return Prix;
+    }
+
+    public void setPrix(double prix) {
+        Prix = prix;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
     private double Prix;
     private int id;
     private String Name;
