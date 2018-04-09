a9787493ff3ea65cf2d191ab76013bdf7da50d65
victorien <victorien.baty@epsi.fr>
2018-04-09 09:08:58 +0200

    implement TestProduct.java

--- src/main/java/src/Product.java
+++ src/main/java/src/Product.java
@@ -1,31 +1,7 @@
 package src;
 
 public class Product {
-    public double getPrix() {
-        return Prix;
-    }
-
-    public void setPrix(double prix) {
-        Prix = prix;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return Name;
-    }
-
-    public void setName(String name) {
-        Name = name;
-    }
-
-    private double Prix;
+    private double prix;
     private int id;
-    private String Name;
+    private String name;
 }
---
+++ src/test/java/TestProduct.java
@@ -0,0 +1,21 @@
+
+    package test;
+
+import org.junit.Test;
+    public class TestProduct {
+        @Test
+        public void insertProduct
+        {
+
+
+        }
+
+        public void insertUser
+        {
+
+
+        }
+
+    }
+
+}
