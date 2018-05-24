09425c4305b0459a8b3677b10a188b70e7ac8a80
victorien <victorien.baty@epsi.fr>
2018-05-24 08:22:03 +0200

    addded some content

--- src/test/java/TestProduct.java
+++ src/test/java/TestProduct.java
@@ -13,7 +13,7 @@ public class TestProduct {
             Assert.assertEquals("voiture",product.getName());
             product.getId();
             product.getPrix();
-
+            //artifactory test
 
 
             /*Product product=new Product();
