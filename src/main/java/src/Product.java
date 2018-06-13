83f99c081958adab2852b5d3da98ad0c7d2ffb43
victorien <victorien.baty@epsi.fr>
2018-06-13 11:26:34 +0200

    debugging orm

--- src/main/java/src/Product.java
+++ src/main/java/src/Product.java
@@ -1,10 +1,8 @@
 package src;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
+@Table
 @Entity
 public class Product {
     @Id
